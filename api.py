c1f008c47aa13e96ceca3f340cd2f6a0fa8c5f54
qo9skei3owow <145287597+293jOse0ejd8du@users.noreply.github.com>
2024-12-26 18:19:17 +0100

    Update api.py

--- api.py
+++ api.py
@@ -5,16 +5,27 @@ from base64 import b64decode
 app = Flask(__name__)
 
 def dynamicLV(url):
+    print(f"Input URL: {url}") # Debug print
     try:
-        # Split the URL by 'r=' and get the last part
         parts = url.split('r=')
+        print(f"Parts after split: {parts}") # Debug print
         if len(parts) > 1:
             base64_string = parts[-1]
+            print(f"Base64 string before unquote: {base64_string}") # Debug print
             decoded_base64 = unquote(base64_string)
-            return b64decode(decoded_base64).decode('utf-8')
+            print(f"Base64 string after unquote: {decoded_base64}") # Debug print
+            try:
+                result = b64decode(decoded_base64).decode('utf-8')
+                print(f"Decode successfull with {result}") # Debug print
+                return result
+            except Exception as b64_err:
+                 print(f"b64decode Error: {b64_err}") # Debug print
+                 return None
         else:
-          return None
-    except Exception:
+            print("URL does not contain 'r='")  # Debug print
+            return None
+    except Exception as e:
+        print(f"Overall Exception Error: {e}") # Debug print
         return None
 
 @app.route('/api/dlv', methods=['GET'])
